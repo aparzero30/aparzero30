4771aeedaa9e7ad9c5ba2f3150fa0ffa4b1ee454
aparzero <jimcin30@gmail.com>
2022-11-26 19:59:08 +0800

    made header

---
+++ index.js
@@ -0,0 +1,31 @@
+
+    let header = document.getElementById("menu");
+    let menuicon = document.getElementById("menu-icon");
+   
+    menuicon.onclick = function(){
+
+        header.classList.toggle("openmenu")
+
+        if(menu.classList.contains("openmenu")){
+            menuicon.innerHTML ="close";  
+         }
+         else{
+             menuicon.innerHTML="menu";
+         }
+ 
+        
+     
+
+
+       
+
+
+
+
+
+
+
+
+    }
+    
+
